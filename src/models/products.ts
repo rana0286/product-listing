e3652bcebfc2c4beaef31550bd6853813f18ff81
Vijay Rana <vijay.rana@Vijays-MacBook-Pro.local>
2025-08-15 23:14:23 +0530

    Implemented error and no data state UI

--- src/models/products.ts
+++ src/models/products.ts
@@ -11,7 +11,7 @@ export async function getProducts(): Promise<{
     await new Promise((resolve) => setTimeout(resolve, 500));
 
     // To simulate the error uncomment next line
-    throw new Error("Unable to fetch product list");
+    //throw new Error("Unable to fetch product list");
     return { error: false, data: products };
   } catch (err) {
     return { error: true, data: null };
